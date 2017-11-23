5af452c362f5b6cb2c3ff1a679aba8b65fb7dbba
Linus Alpsten <linus.alpsten@telia.com>
2017-11-23 12:50:49 +0100

    add moduleactivity model

--- Learny/Models/ModuleActivity.cs
+++ Learny/Models/ModuleActivity.cs
@@ -1,6 +1,27 @@
-﻿namespace Learny.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Learny.Models
 {
     public class ModuleActivity
     {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        public int CourseModuleId { get; set; }
+
+        public int ActivityTypeId { get; set; }
+
+        public virtual ActivityType ActivityType { get; set; }
     }
 }
