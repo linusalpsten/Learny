423f78491c213e5b859d0d8359f79ec526f12753
Linus Alpsten <linus.alpsten@telia.com>
2017-11-23 12:44:47 +0100

    add coursemodule model

--- Learny/Models/CourseModule.cs
+++ Learny/Models/CourseModule.cs
@@ -1,6 +1,26 @@
-﻿namespace Learny.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Learny.Models
 {
     public class CourseModule
     {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        public int CourseId { get; set; }
+
+        public virtual ICollection<ModuleActivity> Activities { get; set; }
     }
 }
