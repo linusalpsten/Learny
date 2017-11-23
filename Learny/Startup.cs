d366719098e20c73dbc21ed9b29e946149ecbcba
Linus Alpsten <linus.alpsten@telia.com>
2017-11-23 12:15:54 +0100

    Add project files.

---
+++ Learny/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Learny.Startup))]
+namespace Learny
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
