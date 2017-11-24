25c2795ef7f7cae51ec960b23dd0a3b5d8539068
Egidio Ricchiuti <egisan3@yahoo.com>
2017-11-24 15:02:45 +0100

    Add CreateStudent  Action (from Register),  edit TeacherCreateStudent View, Edit Layout

--- Learny/Controllers/AccountController.cs
+++ Learny/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Learny.Models;
+using Learny.ViewModels;
 
 namespace Learny.Controllers
 {
@@ -142,16 +143,6 @@ namespace Learny.Controllers
             return View();
         }
 
-
-
-
-
-
-
-
-
-
-
         //
         // POST: /Account/Register
         [HttpPost]
@@ -181,6 +172,68 @@ namespace Learny.Controllers
             // If we got this far, something failed, redisplay form
             return View(model);
         }
+
+
+
+        // Student CREATE
+        // GET: /Account/Register
+        [AllowAnonymous]
+        public ActionResult CreateStudent()
+        {
+            return View("TeacherCreateStudent");
+        }
+
+        //
+        // POST: /Account/Register
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateStudent(StudentVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    CourseId = model.CourseId,
+                    Name = model.Name,
+                    UserName = model.Email,
+                    Email = model.Email
+                };
+                var result = await UserManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
+                    // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
+                    // Send an email with this link
+                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+                  //  return RedirectToAction("Index", "Home");
+                    return RedirectToAction("CreateStudent", "Account");
+                }
+                AddErrors(result);
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         //
         // GET: /Account/ConfirmEmail
--- Learny/ViewModels/StudentVM.cs
+++ Learny/ViewModels/StudentVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,18 @@ namespace Learny.ViewModels
     public class StudentVM
     {
         public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string Password { get; set; }
+
+        public int CourseId { get; set; }
     }
 }
