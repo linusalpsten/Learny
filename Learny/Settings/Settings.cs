7e0216bcbbabf69dae11808bf233d4e70eb4b4b1
petrai <petra.lindell@yahoo.se>
2017-11-24 10:26:44 +0100

    Add settings folder and settings class. Move role names and avticity type names to settings class.

---
+++ Learny/Settings/Settings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Learny.Settings
+{
+
+    public class RoleName
+    {
+        public const string teacher = "teacher";
+        public const string student = "student";
+    }
+
+    public class ActivityTypeName
+    {
+        public const string exercise = "Övning";
+        public const string elearning = "E-Learning";
+        public const string lecture = "Föreläsning";
+    }
+}
