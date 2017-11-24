7f0ec84636dae1e840c5521fe9571297e58a474c
Nils Lindstedt <nils.lindstedt@hotmail.com>
2017-11-24 16:02:29 +0100

    Add ModuleViewModel

---
+++ Learny/ViewModels/ModuleViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Learny.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Learny.ViewModels
+{
+    public class ModuleViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Namn")]
+        public string Name { get; set; }
+
+        [Display(Name = "Beskrivning")]
+        public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "Start")]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        [Display(Name = "Slut")]
+        public DateTime EndDate { get; set; }
+
+        public int CourseId { get; set; }
+
+        public virtual ICollection<ModuleActivity> Activities { get; set; }
+
+    }
+}
