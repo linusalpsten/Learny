90b54eba64e909e57e6660134f077891669fc941
Nils Lindstedt <nils.lindstedt@hotmail.com>
2017-11-27 15:11:39 +0100

    Add StudentCourseController and views

---
+++ Learny/Controllers/StudentCourseController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Learny.Models;
+
+namespace Learny.Controllers
+{
+    public class StudentCourseController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: StudentCourse
+        public ActionResult Index()
+        {
+            return View(db.Courses.ToList());
+        }
+
+        // GET: StudentCourse/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Course course = db.Courses.Find(id);
+            if (course == null)
+            {
+                return HttpNotFound();
+            }
+            return View(course);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
