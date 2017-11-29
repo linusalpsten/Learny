c38c6e1b610d2fefd165e579d6a2797b8153db20
Egidio Ricchiuti <egisan3@yahoo.com>
2017-11-29 09:21:46 +0100

    Added Model view to project

--- Learny/Startup.cs
+++ Learny/Startup.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using System.Data.Entity.Infrastructure;
+using System.Xml;
+using Learny.Models;
 
 [assembly: OwinStartupAttribute(typeof(Learny.Startup))]
 namespace Learny
@@ -8,7 +11,24 @@ namespace Learny
     {
         public void Configuration(IAppBuilder app)
         {
+            // Derive the model from the context!
+            //
+            //public void CreateDesignModel()
+            //{
+                using (var context = new ApplicationDbContext())
+                {
+                    XmlWriterSettings settings = new XmlWriterSettings();
+                    settings.Indent = true;
+
+                    using (XmlWriter writer = XmlWriter.Create(@"C:\Dropbox\Visual Studio 2017\Projects\LEXICON\Övningar\Final Project - Lerny\Learny\Model.edmx", settings))
+                    {
+                        EdmxWriter.WriteEdmx(context, writer);
+                    }
+                }
+          //  }
             ConfigureAuth(app);
+
+
         }
     }
 }
