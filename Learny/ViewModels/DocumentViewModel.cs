11cea5110e08d98f229135fa027ad612f3a4a6cc
Linus Alpsten <linus.alpsten@telia.com>
2017-12-01 14:14:22 +0100

    add document view model

---
+++ Learny/ViewModels/DocumentViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Learny.ViewModels
+{
+    public class DocumentViewModel
+    {
+        public int? CourseId { get; set; }
+
+        public int? CourseModuleId { get; set; }
+
+        public int? ModuleActivityId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
