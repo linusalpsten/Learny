466fc8421585dcce4e32d6d93552b9aad2c42877
Linus Alpsten <linus.alpsten@telia.com>
2017-12-01 11:37:50 +0100

    add gethash and gethashstring functions

--- Learny/Controllers/DocumentController.cs
+++ Learny/Controllers/DocumentController.cs
@@ -1,7 +1,10 @@
 ﻿using Learny.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -17,6 +20,21 @@ namespace Learny.Controllers
             var document = db.Documents.FirstOrDefault(d => d.Id == id);
             byte[] documentBytes = System.IO.File.ReadAllBytes(document.Path);
             return File(documentBytes, document.ContentType, document.DisplayName);
+        }
+
+        public static byte[] GetHash(string inputString)
+        {
+            HashAlgorithm algorithm = MD5.Create();  //or use SHA256.Create();
+            return algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputString));
+        }
+
+        public static string GetHashString(string inputString)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in GetHash(inputString))
+                sb.Append(b.ToString("X2"));
+
+            return sb.ToString();
         }
     }
 }
