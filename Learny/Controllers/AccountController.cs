0b2c1b86e1395838bdd9fe3400cfe2980811b345
Nils Lindstedt <nils.lindstedt@hotmail.com>
2017-12-04 09:12:43 +0100

    Add action CreateTeacher

--- Learny/Controllers/AccountController.cs
+++ Learny/Controllers/AccountController.cs
@@ -181,6 +181,45 @@ namespace Learny.Controllers
             return View(model);
         }
 
+        //
+        // GET: /Account/CreateTeacher
+        [Authorize(Roles = RoleName.teacher)]
+        public ActionResult CreateTeacher()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Account/CreateTeacher
+        [HttpPost]
+        [Authorize(Roles = RoleName.teacher)]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateTeacher(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var result = await UserManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
+                    // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
+                    // Send an email with this link
+                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+                    return RedirectToAction("Index", "Home");
+                }
+                AddErrors(result);
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+
+
         #region Student
         [Authorize(Roles = RoleName.teacher)]
         public ActionResult CreateStudentFromNavBar()
