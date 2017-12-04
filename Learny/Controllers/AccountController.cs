ece2db9f97c228b5f580f9c6d95f59fb187deaf4
Nils Lindstedt <nils.lindstedt@hotmail.com>
2017-12-04 10:09:30 +0100

    Add to role teatcher in post

--- Learny/Controllers/AccountController.cs
+++ Learny/Controllers/AccountController.cs
@@ -199,17 +199,21 @@ namespace Learny.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser {
+                    Name = model.Name,
+                    UserName = model.Email,
+                    Email = model.Email };
+
                 var result = UserManager.Create(user, model.Password);
                 if (result.Succeeded)
                 {
-                   // await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+                    var existingUser = UserManager.FindByName(model.Email);
+                    if (!UserManager.IsInRole(existingUser.Id, RoleName.teacher))
+                    {
+                        UserManager.AddToRole(existingUser.Id, RoleName.teacher);
+                    }
 
-                    // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
-                    // Send an email with this link
-                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
 
                     return RedirectToAction("Index", "Home");
                 }
