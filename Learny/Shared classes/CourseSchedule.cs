7df47d29bdb6a8737b1a70fb7d69264e9de1981a
Egidio Ricchiuti <egisan3@yahoo.com>
2017-12-05 15:23:19 +0100

    Add new class for Course Schedule

---
+++ Learny/Shared classes/CourseSchedule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Learny.Shared_classes
+{
+    public class CourseSchedule
+    {
+        // Module start date
+        public DateTime StartDate { get; set; }
+        
+        // Module end date
+        public DateTime EndDate { get; set; }
+
+        // Module name
+        public string ModuleName { get; set; }
+
+        // Activity name
+        public string ActivityName { get; set; }
+    }
+}
