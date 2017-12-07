92aeed660fe01803303ab3998f1c7effb1490dc0
Egidio Ricchiuti <egisan3@yahoo.com>
2017-12-07 16:10:56 +0100

    Changed format for Schedule and highlight current dates activities

--- Learny/SharedClasses/OneScheduleEntry.cs
+++ Learny/SharedClasses/OneScheduleEntry.cs
@@ -1,13 +1,18 @@
 ﻿using Learny.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace Learny.SharedClasses
 {
+    // one entry matches one single day!
     public class OneScheduleEntry
     {
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        //[Display(Name = "Time booked")]
         public DateTime CurrentDate { get; set; }
 
         public string CurrentDay => CurrentDate.DayOfWeek.ToString();
@@ -15,7 +20,7 @@ namespace Learny.SharedClasses
         // Module name
         public string ModuleName { get; set; }
 
-        public List<ModuleActivity> ActivityList { get; set; }
+        public List<string> ActivityNamesList { get; set; }
 
         //// Activity start date
         //public DateTime StartDate { get; set; }
