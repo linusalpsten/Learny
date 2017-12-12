e9c5ed33ed82b03c947fc41481c4ed267c5950fd
Linus Alpsten <linus.alpsten@telia.com>
2017-12-12 09:11:55 +0100

    translate to swedish

--- Learny/Models/CourseModule.cs
+++ Learny/Models/CourseModule.cs
@@ -9,14 +9,18 @@ namespace Learny.Models
         public int Id { get; set; }
 
         [Required]
+        [Display(Name = "Namn")]
         public string Name { get; set; }
 
+        [Display(Name = "Beskrivning")]
         public string Description { get; set; }
 
         [Required]
+        [Display(Name = "Start Datum")]
         public DateTime StartDate { get; set; }
 
         [Required]
+        [Display(Name = "Slut Datum")]
         public DateTime EndDate { get; set; }
 
         public int CourseId { get; set; }
