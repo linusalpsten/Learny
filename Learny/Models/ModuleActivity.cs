041e2ca3723b374a84e113826c5991df0cbe62c5
Linus Alpsten <linus.alpsten@telia.com>
2017-12-12 09:32:08 +0100

    translate to swedish

--- Learny/Models/ModuleActivity.cs
+++ Learny/Models/ModuleActivity.cs
@@ -9,14 +9,18 @@ namespace Learny.Models
         public int Id { get; set; }
 
         [Required]
+        [Display(Name = "Namn")]
         public string Name { get; set; }
 
+        [Display(Name = "Beskrivning")]
         public string Description { get; set; }
 
         [Required]
+        [Display(Name = "Start Datum")]
         public DateTime StartDate { get; set; }
 
         [Required]
+        [Display(Name = "Slut Datum")]
         public DateTime EndDate { get; set; }
 
         public int CourseModuleId { get; set; }
@@ -26,8 +30,10 @@ namespace Learny.Models
         [Display(Name = "Modul")]
         public virtual CourseModule CourseModule { get; set; }
 
+        [Display(Name = "Aktivitets typ")]
         public virtual ActivityType ActivityType { get; set; }
 
+        [Display(Name = "Dokument")]
         public virtual ICollection<Document> Documents { get; set; }
     }
 }
