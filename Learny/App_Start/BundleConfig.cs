113c20e24a83f1677ed9411fd94c661d06846550
Egidio Ricchiuti <egisan3@yahoo.com>
2017-12-12 01:43:05 +0100

    Added capability (and template ) to show smooth drop down in course page

--- Learny/App_Start/BundleConfig.cs
+++ Learny/App_Start/BundleConfig.cs
@@ -10,9 +10,14 @@ namespace Learny
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
+                    //    "~/Client Scripts/DropdownjQuery.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+
+            // Egidio added below:
+
+           
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
@@ -26,6 +31,7 @@ namespace Learny
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+                 //     "~/Content/Dropdown.css"));
         }
     }
 }
