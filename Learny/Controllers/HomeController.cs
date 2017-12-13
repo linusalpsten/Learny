2db70084a7dbe44f79ffe55aeafcd7775e230ea7
Petra Lindell <petra.lindell@yahoo.se>
2017-12-13 22:26:00 +0100

    Add view and action for NavigationLinks

--- Learny/Controllers/HomeController.cs
+++ Learny/Controllers/HomeController.cs
@@ -30,19 +30,50 @@ namespace Learny.Controllers
 
         public ActionResult NavigationLinks(int? courseId = null, int? moduleId = null, int? activityId = null)
         {
-            var documents = new List<Document>();
-            if (courseId != null) documents = db.Documents.Where(d => d.CourseId == courseId).ToList();
-            if (moduleId != null) documents = db.Documents.Where(d => d.CourseModuleId == moduleId).ToList();
-            if (activityId != null) documents = db.Documents.Where(d => d.ModuleActivityId == activityId).ToList();
+            string courseName="";
+            string moduleName="";
+            string activityName="";
+            if (courseId != null)
+            {
+                var course = db.Courses.Where(c => c.Id == courseId).FirstOrDefault();
+                if(course==null) return PartialView("_NavigationalLinksViewModel", new NavigationalLinksViewModel());
 
+                courseName = course.FullCourseName;
+            }
+            if (moduleId != null)
+            {
+                var module = db.Modules.Where(m => m.Id == moduleId).FirstOrDefault();
+                if (module == null) return PartialView("_NavigationalLinksViewModel", new NavigationalLinksViewModel());
+
+                moduleName = module.Name;
+                courseName = module.Course.FullCourseName;
+                
+                courseId = module.Course.Id;
+            }
+            if (activityId != null)
+            {
+                var activity = db.Activities.Where(a => a.Id == activityId).FirstOrDefault();
+                if (activity == null) return PartialView("_NavigationalLinksViewModel", new NavigationalLinksViewModel());
+
+                activityName = activity.Name;
+                moduleName = activity.Module.Name;
+                courseName = activity.Module.Course.FullCourseName;
+
+                moduleId = activity.Module.Id;
+                courseId = activity.Module.Course.Id;
+            }
+            
             var linkData = new NavigationalLinksViewModel()
             {
                 CourseId = courseId,
+                FullCourseName = courseName,
                 ModuleId = moduleId,
+                ModuleName = moduleName,
                 ActivityId = activityId,
+                ActivityName = activityName,
             };
 
-            return PartialView("_NavigationalLinksPartial", linkData);
+            return PartialView("_NavigationalLinksViewModel", linkData);
         }
 
     }
